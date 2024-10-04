eb83cf3cdce2afb61a7433aa50b217c54e34fc4b
edohandoko67 <edohandoko67@gmail.com>
2024-10-04 17:01:39 +0700

    update porto(2)

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,15 +1,30 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: -7.488765911086714, lng: 112.43405752824161};
 
+    // Membuat peta
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
         center: myLatLng
     });
 
+    // Marker untuk lokasi
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Mojokerto, East Java' // Judul Lokasi
     });
+
+    // Mengatur tautan untuk membuka Google Maps
+    var openMapsLink = document.getElementById('open-maps');
+    if (openMapsLink) {
+        openMapsLink.href = `https://www.google.com/maps/@${myLatLng.lat},${myLatLng.lng},17z`;
+    } else {
+        console.error("Element with ID 'open-maps' not found");
+    }
+    
 }
+
+// Inisialisasi peta ketika halaman selesai dimuat
+window.onload = initMap;
+
